753694c7e0391a31cdf7594dea32caa475ade622
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-06-18 19:21:38 -0700

    not sure where im at

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,13 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState, useReducer } from "react";
 
+import { initialState, reducer } from "../reducers/reducer";
 
-const TodoList = (props) => {
-    return ( 
-    <ul class="list-group">
+const TodoList = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newTodoItem, setNewTodoItem] = useState("");
 
-    <li class="list-group-item">Cras justo odio</li>
-  
-  </ul> );
-}
- 
+  const handleChanges = event => {
+    setNewTodoItem(event.target.value);
+  };
+
+  return (
+    <div className="container">
+      <div>
+        <h1 className="emTitle">To Do List</h1>
+        <div>
+          <form>
+            <input value={newTodoItem} onChange={handleChanges} />
+            <button
+              onClick={event => {
+                event.preventDefault();
+                dispatch({ type: "ADD_TODO", payload: newTodoItem });
+                setNewTodoItem("");
+              }}
+              className="toDoButton"
+            >
+              Add New To Do
+            </button>
+          </form>
+        </div>
+        <button
+          onClick={event => {
+            event.preventDefault();
+            dispatch({ type: "CLEAR_COMPLETED" });
+          }}
+        >
+          Clear Completed
+        </button>
+        <div className="toDoListContainer notCompleted">
+          {state.map(item => (
+            <div key={item.id} className="toDoItem">
+              <h3>{item.item}</h3>
+              <input
+                type="checkbox"
+                value={item.completed}
+                onClick={() => {
+                  dispatch({
+                    type: "TOGGLE_COMPLETED",
+                    payload: {
+                      item: item.item,
+                      completed: item.completed,
+                      id: item.id
+                    }
+                  });
+                }}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default TodoList;
